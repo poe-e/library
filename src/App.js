cc4a13d693ab0af450dad47cbb68bf38ddd04bbe
Poe <pwas123@gmail.com>
2022-06-14 18:59:06 -0400

    all basic functionality done, learned about local storage. Probably add more in the future

--- src/App.js
+++ src/App.js
@@ -1,48 +1,53 @@
 import './App.css';
 import React, { useState } from 'react';
-import Popup from './components/addBookPopUp';
+import Popup from './components/Popup';
 
 function App() {
-  const [myLibrary, setLibrary] = useState([
-    {
-    name: 'Test1',
-    author:'Author',
-    pages: '123',
-    bookRead:true
-  },
-  {
-    name: 'Test2',
-    author:'Author',
-    pages: '123',
-    bookRead:false
-  },
-  {
-    name: 'Test3',
-    author:'Author',
-    pages: '123',
-    bookRead:true
-  },
-  {
-    name: 'Test3',
-    author:'Author',
-    pages: '123',
-    bookRead:false
-  }]);
-
+  // const [myLibrary, setLibrary] = useState([
+  //   {
+  //   name: 'Test1',
+  //   author:'Author',
+  //   pages: '123',
+  //   bookRead:true
+  // },
+  // {
+  //   name: 'Test2',
+  //   author:'Author',
+  //   pages: '123',
+  //   bookRead:false
+  // },
+  // {
+  //   name: 'Test3',
+  //   author:'Author',
+  //   pages: '123',
+  //   bookRead:true
+  // },
+  // {
+  //   name: 'Test3',
+  //   author:'Author',
+  //   pages: '123',
+  //   bookRead:false
+  // }]);
+  const [myLibrary, setLibrary] = useState(
+    JSON.parse(localStorage.getItem('library')) || []
+  );
   const [popup, setPopup] = useState(false);
 
-  function togglePopup(){
-    setPopup(!popup);
+  function testFunction(){
+    console.log(JSON.stringify(myLibrary));
   }
 
-  function Book(){
-
-  }
-  function addBookToLibrary(){
-    console.log('adding')
+  function addBookToLibrary(bookToAdd){
+    console.log(bookToAdd)
+    myLibrary.push(bookToAdd);
+    localStorage.setItem('library', JSON.stringify(myLibrary));
+    setLibrary([...myLibrary]);
   }
   function removeBookFromLibrary(e){
+    console.log(e.target);
+    console.log(e.target.id)
     myLibrary.splice(e.target.id, 1);
+    localStorage.setItem('library', JSON.stringify(myLibrary));
     setLibrary([...myLibrary]);
   }
   function changeReadStatus(e){
@@ -52,15 +57,15 @@ function App() {
 
   return (
     <div className="App">
-      <header>
+      <header className='header'>
         <div className="headContainer">
           <h1 style={{fontSize: "50px"}}>library</h1>
-          <button className="button-28">Log in</button>
+          <button className="button-28" onClick={()=>testFunction()}>Log in</button>
         </div>
       </header>
       
       <div className='mainContainer'>
-        <button className='button-29' id='addBook' onClick={()=>{togglePopup();}}>Add Book</button>
+        <button className='button-29' id='addBook' onClick={()=>{setPopup(true);}}>+ Add Book</button>
         <div className='bookContainer'>
             {myLibrary.map((item, index)=>(
               <div key={index} className='book'>
@@ -69,12 +74,13 @@ function App() {
                 <p>{item.pages}</p>
                 <div className='buttons'>
                   <button className={item.bookRead ? 'read' : 'notRead'} id={index} onClick={(e)=>{changeReadStatus(e)}}>{item.bookRead ? 'Read' : 'Unread'}</button>
-                  <button className='remove' onClick={(e)=>removeBookFromLibrary(e)}>Remove</button>
+                  <button className='remove' id={index} onClick={(e)=>removeBookFromLibrary(e)}>Remove</button>
                 </div>
               </div>
             ))}
         </div>
       </div>
+      <Popup trigger={popup} setTrigger={setPopup} addBook={addBookToLibrary}/>
     </div>
   );
 }
---
+++ src/components/Popup.js
@@ -0,0 +1,45 @@
+import './popup.css'
+
+const Popup = (props) =>{
+    function onSubmit(e){
+        e.preventDefault();
+        // console.log(e.target[0].value);
+        // console.log(e.target[1].value);
+        // console.log(e.target[3].checked);
+        // console.log(e.target[3].value === 'on' ? true : false);
+        let book = {
+            name: e.target[0].value,
+            author: e.target[1].value,
+            pages: e.target[2].value,
+            bookRead: e.target[3].checked
+        }
+        props.addBook(book);
+        props.setTrigger(false);
+    }
+    return(props.trigger) ? (
+        <div className="popup"  onClick={(e)=>{
+            //console.log(e.target.className)
+            if(e.target.className === 'popup'){
+                props.setTrigger(false);
+            }
+          }} >
+            <div className="popup-inner">
+                <form onSubmit={(e)=>onSubmit(e)}>
+                    <h3>Add a Book</h3>
+                    <input className='formElements' type='text' id='bookTitle' placeholder='Title' required/>
+                    <input className='formElements' type='text' id='bookAuthor' placeholder='Author' required/>
+                    <input className='formElements' type='number' id='bookPages' placeholder='Pages' required/>
+                    <div className='readBookDiv'>
+                        <label htmlFor='bookRead'>Have you read it?</label>
+                        <input type='checkbox' id='bookRead'/>
+                    </div>
+                    <button className='submitBook'  type='Submit' 
+                    >Submit
+                    </button>
+                </form>
+            </div>
+        </div>
+    ): "";
+}
+
+export default Popup;
